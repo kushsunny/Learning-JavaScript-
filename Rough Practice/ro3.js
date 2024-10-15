c7cc39544d4ea96645324a6bf73e9964ee11bb14
Sunny Kushwaha <kushsunny2002@gmail.com>
2024-10-15 22:49:37 +0530

    a

--- Rough Practice/ro3.js
+++
@@ -1,8 +0,0 @@
-let str = "Donald"; 
-
-// for(let i=0; i<str.length; i++){
-//     if(str[i]=="D"){
-//         str[i]="R"
-//     }
-// }
-// console.log(str);
---
+++ Strings/Practice Problems/pro6.js
@@ -0,0 +1,10 @@
+let arr = ["Amit", "Nrupul","Ankait","Sunny"]; 
+
+//  Count the names starting from A
+let count=0;
+for(let i=0; i<arr.length; i++){
+   for(let j=0; j<arr[i].length; j++){
+        
+   }
+}
+console.log(count);
