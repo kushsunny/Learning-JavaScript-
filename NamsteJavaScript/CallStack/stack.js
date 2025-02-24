afbb79f4f0a6dbae4c6b1751f4cd72929ceedbfd
Sunny Kushwaha <kushsunny2002@gmail.com>
2025-02-25 00:53:48 +0530

    oyehoye

---
+++ NamsteJavaScript/CallStack/stack.js
@@ -0,0 +1,10 @@
+var num = 2; 
+
+function findValues(n){
+    var ans = n*n; 
+    return ans;
+}
+var result1 = findValues(num);
+var result2 = findValues(4);
+console.log(result1)
+console.log(result2)
---
+++ NamsteJavaScript/Hoisting/hos.js
@@ -0,0 +1,9 @@
+console.log(x);
+// var x = 7; 
+function value(){
+    console.log("this is value");
+}
+value();
+console.log(x); 
+console.log(value)
+ 
